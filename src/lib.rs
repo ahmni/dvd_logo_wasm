cd1cda3f30caaf0e05edd38df293edd732a7f5be
Ahmni <ahmni@ku.edu>
2024-04-09 12:37:53 -0500

    Fix dvd logo logic

--- src/lib.rs
+++ src/lib.rs
@@ -11,24 +11,29 @@ use sdl2::Sdl;
 
 #[cfg(target_family = "wasm")]
 pub mod emscripten;
+#[derive(Debug, Clone)]
+pub struct DVDLogo {
+    pub rect: Rect,
+    pub velocity: Velocity,
+    pub color: Color,
+}
+
+#[derive(Debug, Clone)]
+pub struct Velocity {
+    pub x: i32,
+    pub y: i32,
+}
 
 static BLACK: Color = Color::RGB(0, 0, 0);
 
 pub fn main_loop(
     ctx: Rc<RefCell<Sdl>>,
-    rect: Rc<RefCell<Rect>>,
+    dvd_logo: Rc<RefCell<DVDLogo>>,
     canvas: Rc<RefCell<WindowCanvas>>,
 ) -> impl FnMut() {
     let mut events = ctx.borrow_mut().event_pump().unwrap();
-    let mut dvd_logo = DVDLogo {
-        position: Position { x: 0, y: 0 },
-        velocity: Velocity { x: -2, y: -2 },
-        width: 140,
-        height: 100,
-        color: Color::RGB(0, 255, 255),
-    };
-
     move || {
+        let mut dvd_logo = dvd_logo.borrow_mut();
         let mut hit_counter = 0;
         for event in events.poll_iter() {
             match event {
@@ -39,42 +44,18 @@ pub fn main_loop(
                 } => {
                     process::exit(1);
                 }
-                Event::KeyDown {
-                    keycode: Some(Keycode::Left),
-                    ..
-                } => {
-                    rect.borrow_mut().x -= 10;
-                }
-                Event::KeyDown {
-                    keycode: Some(Keycode::Right),
-                    ..
-                } => {
-                    rect.borrow_mut().x += 10;
-                }
-                Event::KeyDown {
-                    keycode: Some(Keycode::Up),
-                    ..
-                } => {
-                    rect.borrow_mut().y -= 10;
-                }
-                Event::KeyDown {
-                    keycode: Some(Keycode::Down),
-                    ..
-                } => {
-                    rect.borrow_mut().y += 10;
-                }
                 _ => {}
             }
         }
 
         // The rest of the game loop goes here...
-        if dvd_logo.position.x + dvd_logo.width as i32 >= 800 || dvd_logo.position.x <= 0 {
-            dvd_logo.velocity.x = -dvd_logo.velocity.x;
+        if dvd_logo.rect.x() + dvd_logo.rect.width() as i32 >= 800 || dvd_logo.rect.x <= 0 {
+            dvd_logo.velocity.x *= -1;
             dvd_logo.color = get_random_color();
             hit_counter += 1;
         }
-        if dvd_logo.position.y + dvd_logo.height as i32 >= 600 || dvd_logo.position.y <= 0 {
-            dvd_logo.velocity.y = -dvd_logo.velocity.y;
+        if dvd_logo.rect.y() + dvd_logo.rect.height() as i32 >= 600 || dvd_logo.rect.y <= 0 {
+            dvd_logo.velocity.y *= -1;
             dvd_logo.color = get_random_color();
             hit_counter += 1;
         }
@@ -86,45 +67,20 @@ pub fn main_loop(
         // Uncomment to visualize pattern
         //points.push(Point::new(dvd_logo.position.x, dvd_logo.position.y));
 
-        dvd_logo.position.x += dvd_logo.velocity.x;
-        dvd_logo.position.y += dvd_logo.velocity.y;
-
         //texture.set_color_mod(dvd_logo.color.r, dvd_logo.color.g, dvd_logo.color.b);
 
-        rect.borrow_mut().x = dvd_logo.position.x;
-        rect.borrow_mut().y = dvd_logo.position.y;
-        rect.borrow_mut().w = dvd_logo.width as i32;
-        rect.borrow_mut().h = dvd_logo.height as i32;
+        dvd_logo.rect.x += dvd_logo.velocity.x;
+        dvd_logo.rect.y += dvd_logo.velocity.y;
 
         let _ = canvas.borrow_mut().set_draw_color(BLACK);
         let _ = canvas.borrow_mut().clear();
         let _ = canvas.borrow_mut().set_draw_color(dvd_logo.color);
-        let _ = canvas.borrow_mut().fill_rect(rect.borrow().clone());
+        let _ = canvas.borrow_mut().fill_rect(dvd_logo.rect.clone());
         let _ = canvas.borrow_mut().present();
     }
 }
 
 use rand::Rng;
-
-struct DVDLogo {
-    position: Position,
-    velocity: Velocity,
-    width: u32,
-    height: u32,
-    color: Color,
-}
-
-#[derive(Debug)]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Debug)]
-struct Velocity {
-    x: i32,
-    y: i32,
-}
 
 fn get_random_color() -> Color {
     Color::RGB(
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
+use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 
-use hello_rust_sdl2_wasm::main_loop;
+use hello_rust_sdl2_wasm::{main_loop, DVDLogo, Velocity};
 
 // Resources
 //     https://developer.mozilla.org/en-US/docs/WebAssembly/Rust_to_Wasm
@@ -20,41 +21,45 @@ fn main() {
     let ctx = sdl2::init().unwrap();
     let video_ctx = ctx.video().unwrap();
 
-    let window  = match video_ctx
-        .window("Hello, Rust / SDL2 / WASM!", 640, 480)
+    let window = match video_ctx
+        .window("Hello, Rust / SDL2 / WASM!", 800, 600)
         .position_centered()
         .opengl()
-        .build() {
+        .build()
+    {
         Ok(window) => window,
-        Err(err)   => panic!("failed to create window: {}", err)
+        Err(err) => panic!("failed to create window: {}", err),
     };
 
-    let canvas = match window
-        .into_canvas()
-        .present_vsync()
-        .build() {
+    let canvas = match window.into_canvas().present_vsync().build() {
         Ok(canvas) => canvas,
-        Err(err) => panic!("failed to create canvas: {}", err)
+        Err(err) => panic!("failed to create canvas: {}", err),
     };
-
-    let rect = Rect::new(0, 0, 10, 10);
 
     let ctx = Rc::new(RefCell::new(ctx));
-    let rect = Rc::new(RefCell::new(rect));
     let canvas = Rc::new(RefCell::new(canvas));
+    let dvd_logo = Rc::new(RefCell::new(DVDLogo {
+        rect: Rect::new(0, 0, 140, 100),
+        velocity: Velocity { x: -2, y: -2 },
+        color: Color::RGB(0, 255, 255),
+    }));
 
     #[cfg(target_family = "wasm")]
     use hello_rust_sdl2_wasm::emscripten;
 
     #[cfg(target_family = "wasm")]
-    emscripten::set_main_loop_callback(main_loop(Rc::clone(&ctx), Rc::clone(&rect), Rc::clone(&canvas)));
+    emscripten::set_main_loop_callback(main_loop(
+        Rc::clone(&ctx),
+        Rc::clone(&dvd_logo),
+        Rc::clone(&canvas),
+    ));
 
     #[cfg(not(target_family = "wasm"))]
     {
         use std::thread::sleep;
         use std::time::Duration;
         loop {
-            main_loop(Rc::clone(&ctx), Rc::clone(&rect), Rc::clone(&canvas))();
+            main_loop(Rc::clone(&ctx), Rc::clone(&dvd_logo), Rc::clone(&canvas))();
             sleep(Duration::from_millis(10))
         }
     }
