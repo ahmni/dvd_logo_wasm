22a826aa5d04a70d95fb3e5e2bef7dc9e4c49c6d
Ahmni <ahmni@ku.edu>
2024-04-09 12:45:12 -0500

    Fix deployment script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export EMCC_CFLAGS="-s USE_SDL=2"
+set -eu
+
+cargo build --target=wasm32-unknown-emscripten --release
+
+mkdir -p dist
+#cp target/asmjs-unknown-emscripten/release/hello-rust-sdl2-wasm.js dist
+
+cp target/wasm32-unknown-emscripten/release/hello_rust_sdl2_wasm.wasm dist
+cp target/wasm32-unknown-emscripten/release/hello-rust-sdl2-wasm.js dist
+cp index.html dist
--- setup_web.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p dist
-cp target/asmjs-unknown-emscripten/release/hello-rust-sdl2-wasm.wasm dist
-cp target/asmjs-unknown-emscripten/release/hello-rust-sdl2-wasm.js dist
-cp index.html dist
